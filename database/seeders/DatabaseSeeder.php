3e7b6ea91357a327e12cbe7642f411a57cbf1f85
daffaya <daffapasya21@gmail.com>
2022-12-07 19:49:32 +0700

    template

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       User::create([
+           'name' => 'Bioskopy',
+           'email' => 'bioskopy@gmail.com',
+           'password' => Hash::make('123')
+       ]);
     }
 }
