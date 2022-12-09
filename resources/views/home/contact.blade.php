75778f16b0cca7985b8ef6b05441d3061642baad
daffaya <daffapasya21@gmail.com>
2022-12-09 08:21:17 +0700

    navbar

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -19,46 +19,67 @@
 
     <title>Company</title>
 
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow shadow-sm fixed-top fy-3">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="#"><span class="primary">COM</span>PANY</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link fw-bolder" href="/">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link fw-bolder dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            About
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="about">About Us</a></li>
-                            <li><a class="dropdown-item" href="team">Team</a></li>
-                            <li>
-                                <a class="dropdown-item" href="testimonials">Testimonials</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link fw-bolder" href="services">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link fw-bolder" href="portfolio">Portfolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link fw-bolder active" href="contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- end navbar -->
+   <!-- navbar -->
+   <nav class="navbar navbar-expand-lg navbar-light bg-white shadow shadow-sm fixed-top fy-3">
+    <div class="container">
+    <a class="navbar-brand fw-bold" href="#"><span class="primary">COM</span>PANY</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <a class="nav-link fw-bolder" href="/">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link fw-bolder dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            Lingkup Layanan
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li>
+                <a class="dropdown-item" href="services">Jasa Nasehat dan Konsultasi Rekayasa
+                Teknik</a>
+            </li>
+            <li>
+                <a class="dropdown-item" href="services">Jasa Desain Rekayasa untuk Pekerjaan
+                Teknik Sipil Transportasi</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="services">Jasa Nasehat dan Konsultasi Jasa Rekayasa
+                  Konstruksi</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="services">Jasa Desain Rekayasa Lainnya</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="services">Jasa Perencanaan dan Perancangan
+                  Perkotaan</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="services">Jasa Konsultasi Lingkungan</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="services">Jasa Nasehat dan Konsultasi Jasa Rekayasa
+                  Konstruksi</a>
+              </li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link fw-bolder" href="portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bolder active" href="about">About</a>
+        </li>
+        {{-- <li class="nav-item">
+            <a class="nav-link fw-bolder active" href="contact">Contact</a>
+        </li> --}}
+        </ul>
+    </div>
+    </div>
+</nav>
+<!-- end navbar -->
 
     <!-- breadcumbs  -->
     <div class="breadcumbs py-2">
