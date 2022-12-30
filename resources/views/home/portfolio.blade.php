8aaf5c54ff50e2038a4e16faf34c868770746051
daffaya <daffapasya21@gmail.com>
2022-12-30 07:45:35 +0700

    portfolio cpyright

--- resources/views/home/portfolio.blade.php
+++ resources/views/home/portfolio.blade.php
@@ -281,13 +281,13 @@
   </a>
   <!-- Whatsapp -->
 
-    <script src="assets/vendor/jquery/jquery-3.6.0.min.js"></script>
-    <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/fontawesome/js/all.min.js"></script>
-    <script src="assets/vendor/masonry/masonry.pkgd.min.js"></script>
-    <script src="assets/vendor/aos/dist/aos.js"></script>
-    <script src="assets/vendor/isotope/isotope.pkgd.min.js"></script>
-    <script src="assets/js/app.js"></script>
-    </body>
+  <script src="assets/vendor/jquery/jquery-3.6.0.min.js"></script>
+  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/fontawesome/js/all.min.js"></script>
+  <script src="assets/vendor/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/vendor/aos/dist/aos.js"></script>
+  <script src="assets/vendor/isotope/isotope.pkgd.min.js"></script>
+  <script src="assets/js/app.js"></script>
+  </body>
 
 </html>
