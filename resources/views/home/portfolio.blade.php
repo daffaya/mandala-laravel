04d164d43b9a09918c37af3a7315218db12e949b
daffaya <daffapasya21@gmail.com>
2022-12-30 07:43:42 +0700

    about, porto footer copyright

--- resources/views/home/portfolio.blade.php
+++ resources/views/home/portfolio.blade.php
@@ -130,150 +130,150 @@
     </div>
     <!-- end teams -->
 
-  <!-- footer -->
-  <footer>
-    <div class="footer-top text-white p-5 " style="background-color: #101820">
-      <div class="container-fluid">
-        <div class="row py-3 px-5 text-center text-lg-start">
-          <a href="/" class="fs-3 fw-light text-white mb-3 mb-lg-5 text-decoration-none">
-            <img src="assets/img/Logo putih.png" alt="" width="150px">
-          </a>
+<!-- footer -->
+<footer>
+  <div class="footer-top text-white p-5 " style="background-color: #101820">
+    <div class="container-fluid">
+      <div class="row py-3 px-5 text-center text-lg-start">
+        <a href="/" class="fs-3 fw-light text-white mb-3 mb-lg-5 text-decoration-none">
+          <img src="assets/img/Logo putih.png" alt="" width="150px">
+        </a>
 
-            {{-- Explore Section --}}
-            <div class="col-2 my-5 my-lg-0 text-white">
-              <h4 class="fs-4 fw-boldtext-white" style="padding-bottom: 24px">Explore</h2>
-                <ul class="list-group list-unstyled">
-                  <li class="list-item" style="padding-bottom: 24px">
-                    <a href="/" class="text-decoration-none text-white">
-                      Home
-                    </a>
-                  </li>
-                  <li class="list-item" style="padding-bottom: 24px">
-                    <a href="/layanan" class="text-decoration-none text-white">
-                      Lingkup Layanan
-                    </a>
-                  </li>
-                  <li class="list-item" style="padding-bottom: 24px">
-                    <a href="/portofolio" class="text-decoration-none text-white">
-                      Portofolio
-                    </a>
-                  </li>
-                  <li class="list-item" style="padding-bottom: 24px">
-                    <a href="/about" class="text-decoration-none text-white">
-                      About
-                    </a>
-                  </li>
-                </ul>
-            </div>
-            {{-- <h4 class="fw-bold">{{$contact->name}}</h2> --}}
-
-              
-          {{-- Lingkup Layanan Section --}}
-          <div class="col-4 my-5 my-lg-0 text-white">
-            <h4 a href="/layanan" class="fs-4 fw-boldtext-white">Lingkup Layanan</h2>
+          {{-- Explore Section --}}
+          <div class="col-2 my-5 my-lg-0 text-white">
+            <h4 class="fs-4 fw-boldtext-white" style="padding-bottom: 24px">Explore</h2>
               <ul class="list-group list-unstyled">
-                @foreach($services as $service)
-                <li class="list-item">
-                  <a href="" class="text-decoration-none text-white">
-                    <i class="fa fa-chevron-right primary"></i>
-                    {{$service->title}}
+                <li class="list-item" style="padding-bottom: 24px">
+                  <a href="/" class="text-decoration-none text-white">
+                    Home
                   </a>
                 </li>
-                @endforeach
+                <li class="list-item" style="padding-bottom: 24px">
+                  <a href="/layanan" class="text-decoration-none text-white">
+                    Lingkup Layanan
+                  </a>
+                </li>
+                <li class="list-item" style="padding-bottom: 24px">
+                  <a href="/portofolio" class="text-decoration-none text-white">
+                    Portofolio
+                  </a>
+                </li>
+                <li class="list-item" style="padding-bottom: 24px">
+                  <a href="/about" class="text-decoration-none text-white">
+                    About
+                  </a>
+                </li>
               </ul>
           </div>
+          {{-- <h4 class="fw-bold">{{$contact->name}}</h2> --}}
 
-          {{-- Contacts Section --}}
-          <div class="col-12 col-lg my-5 my-lg-0 text-white">
-            <h1 class="fs-4 fw-boldtext-white">Contact</h1>
-            <div class="mt-4 pe-lg-5 d-flex flex-column">
-                <div
-                  class="
-                    d-flex
-                    flex-row
-                    gap-3
-                    justify-content-center justify-content-lg-start
-                    align-items-start
-                    "
-                >
-                  <img src="assets/img//Call.svg" alt="phone"/>
-                    <p class="fs-6">{{$contact->telepon}}</p>
-                </div>
-                
-                <div
-                  class="
-                    d-flex
-                    flex-row
-                    gap-3
-                    justify-content-center justify-content-lg-start
-                    align-items-start
-                  "
-                >
-                  <img src="assets/img//Message.svg" alt="email"/>
-                    <p class="fs-6">{{$contact->email}}</p>
-                </div>
-                
-                <div
-                  class="
-                    d-flex
-                    flex-row
-                    gap-3
-                    justify-content-center justify-content-lg-start
-                    align-items-start
-                  "
-                >
-                  <img src="assets/img//Location.svg" alt="location"/>
-                    <p class="fs-6">
-                      {{$contact->alamat}}
-                          </p>
-                      </div>
-                  </div>
-          </div>
-
-          <div class="col-12 col-lg my-5 my-lg-0 text-white">
-
+            
+        {{-- Lingkup Layanan Section --}}
+        <div class="col-4 my-5 my-lg-0 text-white">
+          <h4 a href="/layanan" class="fs-4 fw-boldtext-white">Lingkup Layanan</h2>
             <ul class="list-group list-unstyled">
-              <li class="list-item" style="padding-bottom: 24px">
-                {{-- Embed Maps --}}
-                <div class="mapouter"><div class="gmap_canvas">
-                  <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=267&amp;height=134&amp;hl=en&amp;q=cv mandala projects&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://formatjson.org/">format json</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:134px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:134px;}.gmap_iframe {height:134px!important;}</style>
-                </div> 
+              @foreach($services as $service)
+              <li class="list-item">
+                <a href="" class="text-decoration-none text-white">
+                  <i class="fa fa-chevron-right primary"></i>
+                  {{$service->title}}
+                </a>
               </li>
-              
-              <li class="list-item" style="padding-bottom: 100px">
-                <h1 class="fs-6 text-center text-soft-grey">Wates, Yogyakarta</h1>
-              </li>
-
-              <li class="list-item" style="padding-bottom: 24px" >
-                {{-- Login Button --}}
-                <div class="col">
-                  <div id="action" class="d-flex justify-content-center text-center flex-row">
-                    <span class="align-middle" style="padding-right: 8px; font-size: 16px;">
-                      You are an admin?
-                    </span>
-
-                    <button>
-                      <a href="/login" class="primary-button" target="_blank">Login</a>
-                    </button>
-                  </div>
-                </div>
-              </li>
+              @endforeach
             </ul>
-          </div>          
         </div>
+
+        {{-- Contacts Section --}}
+        <div class="col-12 col-lg my-5 my-lg-0 text-white">
+          <h1 class="fs-4 fw-boldtext-white">Contact</h1>
+          <div class="mt-4 pe-lg-5 d-flex flex-column">
+              <div
+                class="
+                  d-flex
+                  flex-row
+                  gap-3
+                  justify-content-center justify-content-lg-start
+                  align-items-start
+                  "
+              >
+                <img src="assets/img//Call.svg" alt="phone"/>
+                  <p class="fs-6">{{$contact->telepon}}</p>
+              </div>
+              
+              <div
+                class="
+                  d-flex
+                  flex-row
+                  gap-3
+                  justify-content-center justify-content-lg-start
+                  align-items-start
+                "
+              >
+                <img src="assets/img//Message.svg" alt="email"/>
+                  <p class="fs-6">{{$contact->email}}</p>
+              </div>
+              
+              <div
+                class="
+                  d-flex
+                  flex-row
+                  gap-3
+                  justify-content-center justify-content-lg-start
+                  align-items-start
+                "
+              >
+                <img src="assets/img//Location.svg" alt="location"/>
+                  <p class="fs-6">
+                    {{$contact->alamat}}
+                        </p>
+                    </div>
+                </div>
+        </div>
+
+        <div class="col-12 col-lg my-5 my-lg-0 text-white">
+
+          <ul class="list-group list-unstyled">
+            <li class="list-item" style="padding-bottom: 24px">
+              {{-- Embed Maps --}}
+              <div class="mapouter"><div class="gmap_canvas">
+                <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=267&amp;height=134&amp;hl=en&amp;q=cv mandala projects&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://formatjson.org/">format json</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:134px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:134px;}.gmap_iframe {height:134px!important;}</style>
+              </div> 
+            </li>
+            
+            <li class="list-item" style="padding-bottom: 100px">
+              <h1 class="fs-6 text-center text-soft-grey">Wates, Yogyakarta</h1>
+            </li>
+
+            <li class="list-item" style="padding-bottom: 24px" >
+              {{-- Login Button --}}
+              <div class="col">
+                <div id="action" class="d-flex justify-content-center text-center flex-row">
+                  <span class="align-middle" style="padding-right: 8px; font-size: 16px;">
+                    You are an admin?
+                  </span>
+
+                  <button>
+                    <a href="/login" class="primary-button" target="_blank">Login</a>
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>          
       </div>
     </div>
-    
+  </div>
+  
 
-    <div class="footer-down text-white px-5 py-3" style="background-color: #101820">
-        <div class="copyright">
-          <h1 class="fs-6 fw-bold text-center text-soft-grey">
-            Copyright © Mandala Projects
-          </h1>
-        </div>
-    </div>
-  </footer>
-  <!-- end footer  -->
+  <div class="footer-down text-white px-5 py-3" style="background-color: #101820">
+      <div class="copyright">
+        <h1 class="fs-6 fw-bold text-center text-soft-grey">
+          Copyright © Mandala Projects
+        </h1>
+      </div>
+  </div>
+</footer>
+<!-- end footer  -->
 
   <!-- Whatsapp -->
   <a href="https://api.whatsapp.com/send?phone=6282227059970" target="_blank" class="btn-to-top p-3" style="border-radius: 50%;">
