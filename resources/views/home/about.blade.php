af1a6ee627973442e6b526714ff732761bdca81a
daffaya <daffapasya21@gmail.com>
2023-01-10 11:53:57 +0700

    change logo

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -121,7 +121,7 @@
         @endif
         @if ($message = Session::get('success'))
         <div class="alert alert-success alert-block">
-         <button type="button" class="close" data-dismiss="alert">×</button>
+         <button type="button" class="close" data-dismiss="alert"></button>
                 <strong>{{ $message }}</strong>
         </div>
         @endif
